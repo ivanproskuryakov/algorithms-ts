46f2720e5ff9543f2f7d5883693effada5073fed
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2023-02-23 17:32:36 +0400

    solution for https://leetcode.com/problems/4sum/

--- src/hashtable/fourSum.ts
+++ src/hashtable/fourSum.ts
@@ -4,51 +4,41 @@
 // 0 <= a, b, c, d < n
 // a, b, c, and d are distinct.
 // nums[a] + nums[b] + nums[c] + nums[d] == target
-
 // You may return the answer in any order.
 
 // https://leetcode.com/problems/4sum/
 
-function fourSum(nums: number[], target: number): number[][] {
-  const numbers: number[][] = [];
+function fourSum(nums: number[], _target: number): number[][] {
+  const numbers:number[][] = [];
+  const hashses:string[] = []
 
-  console.log(nums);
-  console.log(target);
+  nums = nums.sort((a, b) => a - b);
 
-  for (let x = 0; x < nums.length; x++) {
-    const n = [nums[x]];
+  for (let i = 0; i < nums.length; i++) {
+    let answer: number[] = [];
 
-    for (let y = 0; y < nums.length; y++) {
-      const sum = n.reduce((r, v) => r + v, 0);
+    for (let x = 0; x < nums.length; x++) {
+      if (x !== i) {
+        if (answer.length < 2) { // less then half
+          answer.push(nums[x]);
+        } else {
+          if (answer.includes(-nums[x]) || answer.includes(nums[x])) {
+            answer.push(nums[x]);
+          }
+        }
+      }
+    }
 
-      console.log(sum);
+    const hash = answer.toString();
 
-      // const sum = numbers[x].includes(nums[y]);
-
-      // if (x !== y && !found1) {
-      //   let found2 = false;
-
-      //   for (let z = 0; z < numbers.length; z++) {
-      //     const s1 = numbers[z].join('');
-      //     const s2 = [...numbers[x], ...[nums[y]]].join('');
-
-      //     if (s1 === s2) {
-      //       found2 = true;
-      //     }
-      //   }
-
-      //   if (!found2) {
-      //     numbers[x].push(nums[y]);
-      //     numbers[x].sort((a, b) => a - b);
-      //   }
-      // }
+    if (!hashses.includes(hash) && answer.length === 4 ) {
+      numbers.push(answer);
+      hashses.push(hash);
     }
   }
 
   return numbers;
 };
 
-console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
-
-// console.log(fourSum([1, 0, -1, 0, -2, 2], 0)); // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
-// console.log(fourSum([2, 2, 2, 2, 2], 8)); // [[2,2,2,2]]
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0)); // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+console.log(fourSum([2, 2, 2, 2, 2], 8)); // [[2,2,2,2]]
